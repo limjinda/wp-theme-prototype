97fc717675d7716fc17be44838fb73d360f9fcd9
Jirayu Limjinda <jir4yu.l@gmail.com>
2017-11-03 17:03:37 +0700

    add favicons

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 	<head>
 	  <meta charset="utf-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() ?>/img/favicons/apple-touch-icon.png">
+	  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-32x32.png">
+	  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri() ?>/img/favicons/favicon-16x16.png">
+	  <link rel="manifest" href="<?php echo get_template_directory_uri() ?>/img/favicons/manifest.json">
+	  <link rel="mask-icon" href="<?php echo get_template_directory_uri() ?>/img/favicons/safari-pinned-tab.svg" color="#eac040">
+	  <meta name="theme-color" content="#ffffff">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class() ?>>
