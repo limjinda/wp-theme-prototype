372fcf4fea0674ffec0520f1d95edc44c573aee5
Jirayu Limjinda <jir4yu.l@gmail.com>
2017-11-03 16:48:28 +0700

    add default template

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<!-- happy coding :) -->
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<?php wp_head(); ?>
+	</head>
+	<body>
+		<h1>Hello</h1>
+		<p>Thanks for using our starter theme!</p>
+		<?php wp_footer(); ?>
+	</body>
+</html>
